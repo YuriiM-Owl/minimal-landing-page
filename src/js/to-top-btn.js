3a9fa541845929a587e5a44a7a92fb6f0de224a9
YuriiM-Owl <129115158+YuriiM-Owl@users.noreply.github.com>
2023-08-16 04:31:03 +0300

    disappear scroll-up btn

--- src/js/to-top-btn.js
+++ src/js/to-top-btn.js
@@ -1,3 +1,4 @@
+let scrollPosY = 0;
 const btnScrollToTop = document.querySelector('.scroll-to-top-btn');
 
 btnScrollToTop.addEventListener('click', () => {
@@ -6,4 +7,16 @@ btnScrollToTop.addEventListener('click', () => {
     left: 0,
     behavior: 'smooth',
   });
+});
+
+window.addEventListener('scroll', () => {
+  let windowPosY = window.scrollY;
+  if (windowPosY < scrollPosY) {
+    btnScrollToTop.classList.add('scroll-up-visible');
+    btnScrollToTop.classList.remove('scroll-up-hidden');
+  } else {
+    btnScrollToTop.classList.add('scroll-up-hidden');
+    btnScrollToTop.classList.remove('scroll-up-visible');
+  }
+  scrollPosY = windowPosY;
 });
